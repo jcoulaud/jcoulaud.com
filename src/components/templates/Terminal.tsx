20af46d838cc67abe45d904e5689081a2f5a19fc
Julien Coulaud <j.coulaud@gmail.com>
2023-10-27 14:45:25 +0100

    feat: change colors of JSON + change favicon

--- src/components/templates/Terminal.tsx
+++ src/components/templates/Terminal.tsx
@@ -1,36 +1,82 @@
+import React from 'react';
+
 import { TerminalHeader } from '@/components/organisms';
 
-export const Terminal = () => {
-  return (
-    <div className='w-full max-w-lg md:max-w-xl'>
-      <div className='coding inverse-toggle px-2 sm:px-5 pt-4 shadow-lg text-gray-100 text-xs sm:text-sm font-mono subpixel-antialiased bg-gray-800 pb-6 rounded-lg leading-normal overflow-hidden'>
-        <div className='top mb-2 flex'>
-          <TerminalHeader />
-        </div>
-        <div className='mt-4 flex'>
-          <span className='text-green-400'>jcoulaud:~$</span>
-          <p className='flex-1 typing items-center pl-2'>cat me.json</p>
-        </div>
-        <div className='mt-1 flex'>
-          <div className='flex-1 typing items-center'>
-            <pre>{JSON}</pre>
-          </div>
+export const Terminal: React.FC = () => (
+  <div className='w-full max-w-lg md:max-w-xl'>
+    <div className='coding inverse-toggle px-2 sm:px-5 pt-4 shadow-lg text-gray-100 text-xs sm:text-sm font-mono subpixel-antialiased bg-gray-800 pb-6 rounded-lg leading-normal overflow-hidden'>
+      <div className='top mb-2 flex'>
+        <TerminalHeader />
+      </div>
+      <div className='mt-4 '>
+        <span className='text-blue-400'>~/Work/Dev/jcoulaud</span>
+        <p className='flex-1 typing items-center'>
+          <span className='text-green-400'>❯ cat</span> me.json
+        </p>
+      </div>
+      <div className='flex mt-1'>
+        <div className='flex-1 typing items-center'>
+          <pre className='whitespace-pre-wrap'>
+            {'{'}
+            {renderJsonObject(ME, 1)}
+            {'}'}
+          </pre>
         </div>
       </div>
     </div>
-  );
+  </div>
+);
+
+/*
+ * Utils
+ */
+
+interface JsonObject {
+  [key: string]: any;
+}
+
+const renderJsonValue = (value: any, indentLevel: number): JSX.Element => {
+  if (typeof value === 'object') {
+    return (
+      <>
+        {'{'}
+        {renderJsonObject(value, indentLevel + 1)}
+        {'}'.padStart(indentLevel * 2 + 1)}
+      </>
+    );
+  }
+  return <span className='text-yellow-200'>&quot;{value}&quot;</span>;
 };
 
-const JSON = `{
-  "firstName": "Julien",
-  "lastName": "Coulaud",
-  "email": "hello",
-  "job": {
-    "role": "Senior Full Stack Engineer",
-    "company": "Solcial"
+const renderJsonObject = (obj: JsonObject, indentLevel = 1): JSX.Element => {
+  const subIndent = ' '.repeat(indentLevel * 2);
+  const entries = Object.entries(obj).map(([key, value], index, array) => (
+    <div key={index}>
+      <span className='text-cyan-200'>
+        {subIndent}&quot;{key}&quot;
+      </span>
+      : {renderJsonValue(value, indentLevel)}
+      {index !== array.length - 1 ? ',' : ''}
+    </div>
+  ));
+
+  return <div>{entries}</div>;
+};
+
+/*
+ * JSON object representing me
+ */
+
+const ME: JsonObject = {
+  firstName: 'Julien',
+  lastName: 'Coulaud',
+  email: 'hello',
+  job: {
+    role: 'Senior Full Stack Engineer',
+    company: 'Solcial',
   },
-  "life": {
-    "sideProject": "https://recapbot.xyz",
-    "sport": "Tennis"
-  }
-}`;
+  life: {
+    sideProject: 'https://recapbot.xyz',
+    sport: 'Tennis',
+  },
+};
